ea3f7cbcaf0e9cb4e94fe8dd248e2a12920f0fbb
xiaobaicai <1908654110@qq.com>
2018-12-17 20:15:12 +0800

    修改了location 页面中按钮样式bug

--- app/src/main/java/com/app/bm/bm/fragment/CustomizeFragment.java
+++ app/src/main/java/com/app/bm/bm/fragment/CustomizeFragment.java
@@ -5,14 +5,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.app.bm.bm.R;
 
 public class CustomizeFragment extends Fragment {
 
+    private Button btnStartCustomize;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstancestate){
         View rootView = inflater.inflate(R.layout.fragment_customize,null);
+
+        btnStartCustomize  = (Button)rootView.findViewById(R.id.start_my_customize);
+        btnStartCustomize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
         return rootView;
     }
 }
